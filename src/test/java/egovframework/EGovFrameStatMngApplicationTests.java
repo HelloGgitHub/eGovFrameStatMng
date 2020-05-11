091d2c4117634e084e47126c86df0572d81dcf18
ywgo <jungun8309@gmail.com>
2020-05-11 10:59:13 +0900

    최초버전 ( 사용자 관리 참조 )

---
+++ src/test/java/egovframework/EGovFrameStatMngApplicationTests.java
@@ -0,0 +1,13 @@
+package egovframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGovFrameStatMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
