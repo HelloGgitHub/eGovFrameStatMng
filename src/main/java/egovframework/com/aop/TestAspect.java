735b36d7bc2b0035b5c01cae1f22f5231bb4c54c
wkyokim <dnjsry12#>
2020-06-10 18:00:02 +0900

    수정

---
+++ src/main/java/egovframework/com/aop/TestAspect.java
@@ -0,0 +1,50 @@
+package egovframework.com.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class TestAspect {
+
+
+/*    @Before("execution(* egovframework.com.stat.service.*.*(..))")
+    public void onBeforeHandler(JoinPoint joinPoint) {
+
+        log.info("=============== onBeforeThing");
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("=============== className : "+ className +"=============== methodName : "+ methodName);
+    }*/
+
+
+    @After("execution(* egovframework.com.stat..*Service.select*(..))")
+    public void onAfterHandler(JoinPoint joinPoint) {
+
+        log.info("=============== onAfterHandler");
+
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        String hostName = System.getenv("HOSTNAME");
+        log.info("=============== className : "+ className +"=============== methodName : "+ methodName +"===============hostName:"+hostName);
+
+    }
+
+/*    @AfterReturning(pointcut = "execution(* egovframework.com.stat.dao.*.*(..)))",
+            returning = "str")
+    public void onAfterReturningHandler(ProceedingJoinPoint joinPoint, Object str) {
+        log.info("@AfterReturning : " + str);
+        log.info("=============== onAfterReturningHandler");
+    }*/
+
+/*    @Pointcut("execution(* egovframework.com.stat.service.*Service.select*(..))")
+    public void onPointcut(JoinPoint joinPoint) {
+        log.info("=============== onPointcut");
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("=============== className : "+ className +"=============== methodName : "+ methodName);
+    }*/
+}
