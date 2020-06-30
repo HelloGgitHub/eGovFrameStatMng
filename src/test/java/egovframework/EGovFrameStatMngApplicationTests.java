10e704e08842a05f94740b5fa66420cddb33cafb
wkyokim <dnjsry12#>
2020-06-30 21:10:00 +0900

    수정

--- src/test/java/egovframework/EGovFrameStatMngApplicationTests.java
+++ src/test/java/egovframework/EGovFrameStatMngApplicationTests.java
@@ -1,13 +1,13 @@
-package egovframework;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EGovFrameStatMngApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package egovframework;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EGovFrameStatMngApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
